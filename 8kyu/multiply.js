866b2017a7858f611153d6371fe5aaae4275ec6c
Aaron Butnik <aaronjbutnik@gmail.com>
2023-05-30 19:39:07 -0700

    added new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,11 @@
+// Prompt:
+// This code does not execute properly. Try to figure out why.
+// multiply = function (a, b)
+
+// My solution:
+
+multiply = function (a, b) {
+  return a * b;
+};
+const result = multiply(3, 3);
+console.log(result);
